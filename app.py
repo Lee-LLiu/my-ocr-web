254c6c6915f874e11946385a1e3ff2470927d150
Lee-LLiu <leeliupurpledon@gmail.com>
2026-04-30 15:36:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ st.title("🥬 蔬菜价签自动识别")
 # 在侧边栏配置密钥，更安全
 with st.sidebar:
     st.header("API 配置")
-    app_id = st.text_input("APP_ID", type="a7e8877dba554481becdbf135ce671ee")
-    api_key = st.text_input("API_KEY", type="f59OoSeqYPqMUD1tecX1yeC0")
-    secret_key = st.text_input("SECRET_KEY", type="VF87Je72q32LDe3q75VH9RI7CQbzIb4G")
+    app_id = st.text_input("APP_ID", type="password")
+    api_key = st.text_input("API_KEY", type="password")
+    secret_key = st.text_input("SECRET_KEY", type="password")
 
 # --- 2. 核心逻辑 (保持你之前的 V4 逻辑) ---
 def process_ocr(img_bytes, client):
